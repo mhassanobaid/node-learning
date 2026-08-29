114e989110bd013fb81bdb0065c1ddd10739f50c
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-08-29 23:37:38 +0500

    Day15: mongodb overview

---
+++ Day15-mongo-overview.js
@@ -0,0 +1,28 @@
+// Mongodb is document based operational db built from ground as an alternative to relational db for modern apps. unlike relational,it allows users to store document that map to objects
+
+    // table -> collection
+    //     rows -> documents
+
+                                                        // WHY
+
+  // no sql based
+  // strong support for 
+  //                  aggregation (collecting useful business result from alot of data) 
+  //                  pipelines (do processing step by step)
+  // work on BSON format 
+       // best For Node applicatio
+
+
+  
+                                                        // COMMANDS
+
+// show dbs
+// show collections
+
+// mongosh in window paower shell
+   // show dbs
+   // use db
+          // use collections
+          // db.collectio_name.find({}) // list all docs of collection_name
+          // db.collection_name.insert({})
+
